9d8af97e5dd5a5e0312d108269d1e47f8f262ca0
João (d3z3n0v3) <32623338+hackerftsg@users.noreply.github.com>
2017-12-10 16:01:00 -0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+Copyright 2017 hackerftsg.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+# -*- coding: utf-8 -*-
+
+AD1 = "72.52.0.0"     # IP 1
+AD2 = "72.52.255.255" # IP 2
